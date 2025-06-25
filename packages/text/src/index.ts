0d9b42156c5aec55c2872b2dc6b420201149536a
gongyuzhouchang <1148441101@qq.com>
2025-06-25 10:06:35 +0800

    docs: 删除项目文档
    - 移除了架构总结、新组件指南、发布指南等文档
    - 清理了项目中不再需要的markdown文件

--- packages/text/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Text } from './Text';
-export { TextRenderer, type TextConfig } from './renderer';
-
-// 导出类型定义
-export type { ThemeConfig } from './Text'; 
--- packages/timeline/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TimeLine } from './TimeLine';
-export type { Config, CustomEvent, ThemeConfig, TimeLineConfig } from './TimeLine';
-export { TimeLineRenderer } from './renderer'; 
